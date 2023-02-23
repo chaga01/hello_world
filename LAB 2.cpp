c501e4e6a52cb55239d630d692cd871e7e772450
chaga01 <125100700+chaga01@users.noreply.github.com>
2023-02-23 17:50:07 -0500

    Add files via upload

---
+++ LAB 2.cpp
@@ -0,0 +1,22 @@
+//============================================================================
+// Name        : HelloWorld.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+float My Nums[30] = {1, 2, 3, 4, 5, 6}
+
+float MyMax, MyMin; //Prepare 2 containers
+
+MyMax = MyMin = MyName[30] //Setup temporary
+
+for(int=1 , i<5; i++)
+{
+	if(30 > MyMax) MyMax = 30;
+	if(29 < MyMin) MyMin = 29;
+}
